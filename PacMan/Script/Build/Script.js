2a0b3e6fa1aef4c695242c87e7e8021347eba59e
Matthias Ngo <m-ngo@web.de>
2022-03-29 15:46:37 +0200

    Pacman is running

--- PacMan/Script/Build/Script.js
+++ PacMan/Script/Build/Script.js
@@ -41,14 +41,37 @@ var Script;
     var ƒ = FudgeCore;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
+    let pacman;
+    let direction = ƒ.Vector3.ZERO();
+    let speed = 1 / 60;
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
+        let graph = viewport.getBranch();
+        pacman = graph.getChildrenByName("Pacman")[0];
+        console.log(pacman);
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
+        let posPacman = pacman.mtxLocal.translation;
+        let nearestGridPoint = new ƒ.Vector2(Math.round(posPacman.x), Math.round(posPacman.y));
+        let nearGridPoint = posPacman.toVector2().equals(nearestGridPoint, 2 * speed);
+        if (nearGridPoint) {
+            let directionOld = direction.clone;
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
+                direction.set(1, 0, 0);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]))
+                direction.set(-1, 0, 0);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]))
+                direction.set(0, 1, 0);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]))
+                direction.set(0, -1, 0);
+            if (!direction.equals(directionOld))
+                pacman.mtxLocal.translation = nearestGridPoint.toVector3();
+        }
+        pacman.mtxLocal.translate(ƒ.Vector3.SCALE(direction, speed));
         viewport.draw();
         ƒ.AudioManager.default.update();
     }
